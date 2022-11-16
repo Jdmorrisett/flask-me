1efae9b6e95f9b6f262ac874b6bc0322678d96dc
Jdmorrisett <jdmorrisett2@gmail.com>
2022-11-16 09:54:59 -0500

    ‼️ BREAKING: Not ready for merge yet. Needs CSS work.

--- flask_app.py
+++ flask_app.py
@@ -3,5 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def welcome():
+def home():
+    return render_template('home.html')
+
+@app.route("/resume")
+def resume():
     return render_template('resume.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
