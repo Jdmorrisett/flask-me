1ebb58f19e4ad11ff644d7e20ba0fec95e95fb92
Jdmorrisett <jdmorrisett2@gmail.com>
2022-11-16 21:01:34 -0500

    seperate pages, add header/sitemap, cicd phase1

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from flask_navigation import Navigation
 
 app = Flask(__name__)
+nav = Navigation(app)
+
+nav.Bar('top', [
+    nav.Item('Home', 'home'),
+    nav.Item('Resume', 'resume'),
+    nav.Item('Contact', 'contact')
+])
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route("/resume")
 def resume():
