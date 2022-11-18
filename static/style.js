e2a40ce2aa52f08ea5a7defef59f7b11b8334e86
Jdmorrisett <jdmorrisett2@gmail.com>
2022-11-17 23:14:01 -0500

    🐛 FIX: jumping text bug

--- static/style.js
+++ static/style.js
@@ -1,10 +1,10 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
-            entry.target.classList.remove('show');
+            // entry.target.classList.remove('show');
         }
     });
 });
